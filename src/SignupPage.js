0c275197144c3b97d26caf09e7c2a0a825c73c0c
vimarshmishra37 <144466200+vimarshmishra37@users.noreply.github.com>
2023-09-22 13:54:58 +0530

    kjb

---
+++ src/SignupPage.js
@@ -0,0 +1,15 @@
+// SignupPage.js
+import React from 'react';
+
+const SignupPage = () => {
+  // Define the content of the signup page
+  return (
+    <div className="signup-page">
+      <h1>Signup</h1>
+      {/* Add your signup form or content here */}
+    </div>
+  );
+};
+
+export default SignupPage;
+npm
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Check from './check';
-import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
